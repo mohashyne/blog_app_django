a28332389a53d21b385e166e9f7fe64e2e829188
MSA <crystalbluetech@gmail.com>
2025-01-09 20:58:31 +0100

    define model for articles

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField()
+    body = models.TextField
+    date = models.DateTimeField(auto_now_add=True)
+ 
+
+# TODO: add thumbnail later and add author  
