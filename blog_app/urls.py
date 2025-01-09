45da437b90b41a94abd50fabddae86f75e613420
MSA <crystalbluetech@gmail.com>
2025-01-09 16:23:55 +0100

    create html templates and configure dir in settings

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('about/', views.about),
+    path('about/', views.about)
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Hello, World! Welcome to our blog!')
+    # return HttpResponse('Hello, World! Welcome to our blog!')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('About Us')
+    # return HttpResponse('About Us')
+    return render(request, 'about.html')
