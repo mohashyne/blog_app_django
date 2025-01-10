1fe70ea9e78756e20d4f00c0b95deb0f92238f1d
MSA <crystalbluetech@gmail.com>
2025-01-10 16:37:27 +0100

    create migrations and migrate models

--- articles/models.py
+++ articles/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField()
-    body = models.TextField
+    body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
  
 
 # TODO: add thumbnail later and add author  
+
+# NOTE: After creating the model, you need to create a migration file and apply it to the database  
+# python manage.py makemigrations # this will create the migration file
+# python manage.py migrate # this will apply the migration file to the database
