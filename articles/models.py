dbe2b8fcd3244537030de5634943dbc638428522
MSA <crystalbluetech@gmail.com>
2025-01-13 14:23:33 +0100

    create admin and register app in admin

--- articles/models.py
+++ articles/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Article(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    
+
 # This will display the title of the article in the admin panel instead of the default 'Article object'
-# This is a dunder method which is called when you try to print the object, it returns the title of the article and not the object, something like stringify the object.    
+# This is a dunder method which is called when you try to print the object, it returns the title of the article and not the object, something like stringify the object.
 
-def __str__(self):
-    return self.title
- 
+    def __str__(self):
+        return self.title
 
-# TODO: add thumbnail later and add author  
 
-# NOTE: After creating the model, you need to create a migration file and apply it to the database  
+# TODO: add thumbnail later and add author
+
+# NOTE: After creating the model, you need to create a migration file and apply it to the database
 # python manage.py makemigrations # this will create the migration file
 # python manage.py migrate # this will apply the migration file to the database
