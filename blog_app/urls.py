1c0507276de22de455c1a842106147eda212d17d
MSA <crystalbluetech@gmail.com>
2025-01-14 22:59:14 +0100

    static files using using static file

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,10 +2,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),                    # Admin site
-    path('articles/', include('articles.urls')),        # Include articles app URLs
+    # Include articles app URLs
+    path('articles/', include('articles.urls')),
     path('', views.home, name='home'),                  # Root URL (homepage)
     path('about/', views.about, name='about'),          # About page
 ]
+
+urlpatterns += staticfiles_urlpatterns()
