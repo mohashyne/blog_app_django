051685cd24afe108e85302df6a90a20edeee0e64
MSA <crystalbluetech@gmail.com>
2025-02-13 17:21:49 +0100

    usr form creation using auth.forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm # Import the UserCreationForm
 
 # Create your views here.
 def signup(request):
-    return render(request, 'accounts/signup.html')  # Render the signup.html template
+    form = UserCreationForm()  # Create a new instance of the UserCreationForm
+    return render(request, 'accounts/signup.html', {'form': form})  # Render the signup.html template
 
 def login(request):
     return render(request, 'accounts/login.html')  # Render the login.html template
