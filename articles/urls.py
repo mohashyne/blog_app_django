bc29f29ce064a105e564b87ba177fbe91d622ca4
MSA <crystalbluetech@gmail.com>
2025-02-19 20:46:56 +0100

    login protection before user can create article

--- articles/urls.py
+++ articles/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
+    path('create/', views.article_create, name='create'),
     path('', views.article_list, name='article_list'),  # Homepage for articles
     path('about/', views.about, name='about'),          # About page for articles
     path('<slug:slug>/', views.article_detail, name='article_detail'),  # Article detail page
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Article
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Article
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def article_list(request):
@@ -14,3 +15,9 @@ def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+# Require login to create an article
+@login_required(login_url='/accounts/login/')
+def  article_create(request):
+    return render(request, 'articles/article_create.html')
+
